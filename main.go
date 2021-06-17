dfefa844defcd40a2ced1a5870049fb62d5345c5
AccurateApplication <lucas@cederqvist.dev>
2021-06-17 10:24:52 +0200

    Check records and update

--- main.go
+++ main.go
@@ -39,4 +39,39 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Populate struct that will be added as record to CF
+	subDomainRecord := createRecord(config, externalIp, config.Subdomain)
+
+	// TODO: Add this as a function
+	lenRecords := len(dnsRecords)
+	switch {
+	case lenRecords == 0:
+		fmt.Println("No records added for domain, will add")
+		err = postDNSRecord(config, CfVars, zoneID, subDomainRecord)
+		if err != nil {
+			log.Println(err)
+		}
+	case lenRecords >= 1:
+		log.Printf("Found %d records matching domain. Will check", lenRecords)
+		for i, r := range dnsRecords {
+			fmt.Printf("Record: %d\tcontent: %s name: %s, ID: %s\n", i, r.Content, r.Name, r.ID)
+			if r.Content != externalIp {
+
+				// Clear all records
+				err = CfVars.API.DeleteDNSRecord(CfVars.context, zoneID, r.ID)
+				if err != nil {
+					log.Println(err)
+				}
+
+				// Post DNS record with correct ext IP
+				err = postDNSRecord(config, CfVars, zoneID, subDomainRecord)
+				if err != nil {
+					log.Println(err)
+				}
+			} else if r.Content == externalIp {
+				fmt.Println("Found record matching external IP. Will keep.")
+			}
+		}
+	}
+
 }
